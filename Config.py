428112f3ec9c959c78bb672362233c806ded5c0c
gebu <gebu@itu.dk>
2022-08-01 13:15:37 +0200

    initial commit

---
+++ Config.py
@@ -0,0 +1,17 @@
+hate_train_text_path = './hate/train_text.txt'
+hate_train_labels_path = './hate/train_labels.txt'
+
+hate_val_text_path = './hate/val_text.txt'
+hate_val_labels_path = './hate/val_labels.txt'
+
+hate_test_text_path = './hate/test_text.txt'
+hate_test_labels_path = './hate/test_labels.txt'
+
+abortion_train_text_path = './abortion/train_text.txt'
+abortion_train_labels_path = './abortion/train_labels.txt'
+
+abortion_val_text_path = './abortion/val_text.txt'
+abortion_val_labels_path = './abortion/val_labels.txt'
+
+abortion_test_text_path = './abortion/test_text.txt'
+abortion_test_labels_path = './abortion/test_labels.txt'
